4e0398a1e28caefffba25ce15b2c8c4db2945da2
Art Pinch <leonardo906@mail.ru>
2020-12-03 22:11:28 +0300

    Added notes on CyrillicToLatinTransliterator implementation

--- app/src/api21/java/ru/art2000/pager/plugins/CyrillicToLatinTransliterator.kt
+++ app/src/api21/java/ru/art2000/pager/plugins/CyrillicToLatinTransliterator.kt
@@ -4,6 +4,10 @@ import com.ibm.icu.text.Transliterator
 import ru.art2000.pager.models.CommunicatorPlugin
 import ru.art2000.pager.models.PagerMessage
 
+/**
+ * Plugin to transliterate cyrillic symbols to latin. This implementation
+ * uses external IBM icu4j library, as Android version requires minSdkApi 29
+ */
 class CyrillicToLatinTransliterator : CommunicatorPlugin {
 
     override fun transform(message: PagerMessage): PagerMessage {
--- app/src/api29/java/ru/art2000/pager/plugins/CyrillicToLatinTransliterator.kt
+++ app/src/api29/java/ru/art2000/pager/plugins/CyrillicToLatinTransliterator.kt
@@ -4,6 +4,10 @@ import android.icu.text.Transliterator
 import ru.art2000.pager.models.CommunicatorPlugin
 import ru.art2000.pager.models.PagerMessage
 
+/**
+ * Plugin to transliterate cyrillic symbols to latin. This implementation
+ * uses Android built-in version of icu4j
+ */
 class CyrillicToLatinTransliterator : CommunicatorPlugin {
 
     override fun transform(message: PagerMessage): PagerMessage {
